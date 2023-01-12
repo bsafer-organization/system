7d0585d059c33685ef8aeaf90bf317ca3c2b0163
Enzo Seiji <enzoseiji14@gmail.com>
2023-01-11 21:07:16 -0300

    LOTO-537 LOTO-538 border radius

---
+++ packages/tokens/src/border-radius.ts
@@ -0,0 +1,11 @@
+export const borderRadius = {
+  none: '0',
+  sm: '0.125rem',
+  md: '0.25rem',
+  lg: '0.375rem',
+  xl: '0.5rem',
+  '2xl': '0.75rem',
+  '3xl': '1rem',
+  '4xl': '1.5rem',
+  full: '9999px'
+}
