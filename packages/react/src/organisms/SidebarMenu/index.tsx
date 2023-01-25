fb9d6e2de8a83f7ad07a7508789c33fa98fa25e9
Felipe Oliveira <flpoliveira42@gmail.com>
2023-01-25 18:23:11 -0300

    feature: loto-581: adjusts component docs

--- packages/react/src/organisms/SidebarMenu/index.tsx
+++ packages/react/src/organisms/SidebarMenu/index.tsx
@@ -12,15 +12,11 @@ import { MenuItemTooltipProps, SidebarMenuProps } from './types'
 
 export type { SidebarMenuProps } from './types'
 
-function Root({
-  children,
-  isOpen = true,
-  activedRoute: isActiveRoutes
-}: SidebarMenuProps['Root']) {
+function Root(props: SidebarMenuProps['Root']) {
+  const { children, isOpen = true, activedRoute } = props
+
   return (
-    <SidebarMenuContext.Provider
-      value={{ isOpen, activedRoute: isActiveRoutes }}
-    >
+    <SidebarMenuContext.Provider value={{ isOpen, activedRoute }}>
       <RootContainer isOpen={isOpen}>
         <ItemsWrapper>{children}</ItemsWrapper>
       </RootContainer>
@@ -62,7 +58,9 @@ function Item(props: SidebarMenuProps['Item']) {
   )
 }
 
-function MenuItemTooptip({ label, hidden, children }: MenuItemTooltipProps) {
+function MenuItemTooptip(props: MenuItemTooltipProps) {
+  const { label, hidden, children } = props
+
   if (hidden) {
     return <>{children}</>
   }
@@ -81,6 +79,28 @@ function MenuItemTooptip({ label, hidden, children }: MenuItemTooltipProps) {
 }
 
 export const SidebarMenu = {
+  /**
+   * SidebarMenu.Root is a container that envelops the links and guarantees the contracted or expanded operation. It also manages which link is active or not based on a string for comparison
+   *
+   * @param children
+   * @param isOpen
+   * @param activedRoute
+   *
+   * @returns div > ul
+   */
   Root,
+
+  /**
+   * SidebarMenu.Item is an action button intended for use in routes. It is possible to insert a label, an icon and route information. It is also possible to listen to the click event for other tasks.
+   *
+   * @param icon
+   * @param label
+   * @param onClick
+   * @param route
+   * @param isActive
+   * @param focusColor
+   *
+   * @returns li > button
+   */
   Item
 }
--- packages/react/src/organisms/SidebarMenu/types.ts
+++ packages/react/src/organisms/SidebarMenu/types.ts
@@ -3,17 +3,49 @@ import React from 'react'
 import { ItemLinkProps } from './styles'
 
 interface ItemProps {
+  /**
+   * Activating an item leaves it with the active focus color
+   * @default false
+   */
   isActive?: boolean
+  /**
+   * Defines which focus color will be used when the link is active
+   * `primary | secondary | black | undefined`
+   * @default 'primary'
+   */
   focusColor?: ItemLinkProps['focusColor']
+  /**
+   * Define an icon to render aside label
+   * @requires
+   */
   icon: Icon | React.ElementType
+  /**
+   * Define an label to link
+   * @requires
+   */
   label: string
+  /**
+   * Defines what the route argument will be when the link is clicked
+   */
   route?: string
-  onClick?: (path?: string) => void
+  /**
+   * This event takes the route parameter and makes it available for an action
+   */
+  onClick?: (route?: string) => void
 }
 
 interface RootProps {
+  /**
+   * Define is menu is open or close
+   */
   isOpen?: boolean
+  /**
+   * Receiving the active route, an internal comparison is made to find out if the route fits this value, making the link enter in focus
+   */
   activedRoute?: string
+  /**
+   * Component content
+   */
   children?: React.ReactNode
 }
 
