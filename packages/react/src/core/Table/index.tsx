0d01e2f923fdda68776d4615caaed68a7c89d52d
Felipe Oliveira <flpoliveira42@gmail.com>
2023-01-30 18:50:30 -0300

    Table adjustments

--- packages/react/src/core/Table/index.tsx
+++ packages/react/src/core/Table/index.tsx
@@ -1,40 +1,57 @@
 import React from 'react'
 
-import {TableRoot, TableHeader, TableHeaderCellProps, TableBodyCellProps, TableBody } from './styles'
+import { TableBody, TableHeader, TableRoot } from './styles'
 
-export interface RootProps extends React.HTMLAttributes<HTMLTableElement> {
-    /**
-     * If the table rows have background, hasBackground will receive true.
-     * Otherwise, it will receive false.
-     * 
-     * @default 'false'
-     */
-    hasBackground?: boolean
-    /**
-     * Table content
-     */
-    children: React.ReactNode
+export interface RootProps extends React.TableHTMLAttributes<HTMLTableElement> {
+  /**
+   * If the table rows have background, hasBackground will receive true.
+   * Otherwise, it will receive false.
+   *
+   * @default 'false'
+   */
+  hasBackground?: boolean
+  /**
+   * Table content
+   */
+  children: React.ReactNode
 }
 
-export interface CellProps extends React.HTMLAttributes<HTMLTableCellElement> {}
+export interface HeaderCellProps
+  extends React.DetailedHTMLProps<
+    React.ThHTMLAttributes<HTMLTableCellElement>,
+    HTMLTableCellElement
+  > {}
+export interface BodyCellProps
+  extends React.DetailedHTMLProps<
+    React.TdHTMLAttributes<HTMLTableCellElement>,
+    HTMLTableCellElement
+  > {}
 
-export interface RowProps extends React.HTMLAttributes<HTMLTableRowElement> {}
+export interface RowProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLTableRowElement>,
+    HTMLTableRowElement
+  > {}
 
-export interface HeaderAndBodyProps extends React.HTMLAttributes<HTMLTableSectionElement> {}
+export interface HeaderAndBodyProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLTableSectionElement>,
+    HTMLTableSectionElement
+  > {}
 
 /**
  * Render a table element
- * @param TableProps 
+ * @param TableProps
  * @returns `<Table.Root>
  *               children
  *           </Table.Root>`
  */
-const Root = ({children, hasBackground, ...props}: RootProps) => {
-    return(
-        <TableRoot hasBackground={hasBackground!} {...props}>
-            {children}
-        </TableRoot>
-    )
+const Root = ({ children, hasBackground, ...props }: RootProps) => {
+  return (
+    <TableRoot hasBackground={hasBackground!} {...props}>
+      {children}
+    </TableRoot>
+  )
 }
 
 /**
@@ -43,41 +60,29 @@ const Root = ({children, hasBackground, ...props}: RootProps) => {
  * @returns `<Table.Header.Root>
  *               children
  *           </Table.Header.Root>`
- * 
+ *
  * Row: Renders a tr element
- * @param TableRowProps 
+ * @param TableRowProps
  * @returns `<Table.Header.Row>
  *               children
  *           </Table.Header.Row>`
- * 
+ *
  * Cell: Renders a th element
- * @param TableHeaderCellProps 
+ * @param TableHeaderCellProps
  * @returns `<Table.Header.Cell>
  *               children
  *           </Table.Header.Cell>`
  */
 const Header = {
-    Root: ({children, ...props}: HeaderAndBodyProps) => {
-        return(
-            <TableHeader.Root {...props}>
-                {children}
-            </TableHeader.Root>
-        )
-    },
-    Row: ({children, ...props}: RowProps) => {
-        return(
-            <TableHeader.Row {...props}>
-                {children}
-            </TableHeader.Row>
-        )
-    },
-    Cell: ({children, ...props}: CellProps) => {
-        return(
-            <TableHeader.Cell {...props}>
-                {children}
-            </TableHeader.Cell>
-        )
-    }
+  Root: ({ children, ...props }: HeaderAndBodyProps) => {
+    return <TableHeader.Root {...props}>{children}</TableHeader.Root>
+  },
+  Row: ({ children, ...props }: RowProps) => {
+    return <TableHeader.Row {...props}>{children}</TableHeader.Row>
+  },
+  Cell: ({ children, ...props }: HeaderCellProps) => {
+    return <TableHeader.Cell {...props}>{children}</TableHeader.Cell>
+  }
 }
 
 /**
@@ -86,41 +91,29 @@ const Header = {
  * @returns `<Table.Body.Root>
  *               children
  *           </Table.Body.Root>`
- * 
+ *
  * Row: Renders a tr element
- * @param TableRowProps 
+ * @param TableRowProps
  * @returns `<Table.Body.Row>
  *               children
  *           </Table.Body.Row>`
- * 
+ *
  * Cell: Renders a td element
- * @param TableBodyCellProps 
+ * @param TableBodyCellProps
  * @returns `<Table.Body.Cell>
  *               children
  *           </Table.Body.Cell>`
  */
 const Body = {
-    Root: ({children, ...props}: HeaderAndBodyProps) => {
-        return(
-            <TableBody.Root {...props}>
-                {children}
-            </TableBody.Root>
-        )
-    },
-    Row: ({children, ...props}: RowProps) => {
-        return(
-            <TableBody.Row {...props}>
-                {children}
-            </TableBody.Row>
-        )
-    },
-    Cell: ({children, ...props}: CellProps) => {
-        return(
-            <TableBody.Cell {...props}>
-                {children}
-            </TableBody.Cell>
-        )
-    }
+  Root: ({ children, ...props }: HeaderAndBodyProps) => {
+    return <TableBody.Root {...props}>{children}</TableBody.Root>
+  },
+  Row: ({ children, ...props }: RowProps) => {
+    return <TableBody.Row {...props}>{children}</TableBody.Row>
+  },
+  Cell: ({ children, ...props }: BodyCellProps) => {
+    return <TableBody.Cell {...props}>{children}</TableBody.Cell>
+  }
 }
 
-export const Table = {Root, Header, Body}
+export const Table = { Root, Header, Body }
