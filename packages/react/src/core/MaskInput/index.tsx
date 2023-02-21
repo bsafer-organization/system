1bcb5ad839822625c3b91921266ef0cf6608430d
Felipe Oliveira <flpoliveira42@gmail.com>
2023-02-21 10:11:26 -0300

    fix: Some props has been fixed, to apply in applications

--- packages/react/src/core/MaskInput/index.tsx
+++ packages/react/src/core/MaskInput/index.tsx
@@ -1,148 +1,168 @@
 import { Danger, SearchNormal1 } from 'iconsax-react'
-import React from 'react'
 import { NumberFormatValues, OnValueChange } from 'react-number-format'
 import {
   ErrorTextContainer,
+  IconContainer,
   InputContainer,
   InputElement,
   InputElementContainer,
+  InputElementProps,
   InputLabel,
-  OptionalBadge,
-  InputElementProps
+  OptionalBadge
 } from './styles'
 
 export interface MaskInputProps {
-    /**
-     * Input label with id and htmlFor
-     */
-    label: string
-    /**
-     * Render a badge with text 'Opcional'
-     * @default false
-     */
-    optional?: boolean
-    /**
-     * Render a message text below input
-     * @default undefined
-     */
-    error?: string
-    /**
-     * Disable all component
-     * @default false
-     */
-    disabled?: boolean
-    /**
-     * Input placeholder
-     * @default undefined
-     */
-    placeholder?: string
-    /**
-     * Renders an icon to the left of the input. When an icon is not passed as a JSX element The magnifying glass icon is the default
-     * @default undefined
-     */
-    startIcon?: boolean | JSX.Element
-    /**
-     * Renders an icon to the right of the input. When an icon is not passed as a JSX element The magnifying glass icon is the default
-     * @default undefined
-     */
-    endIcon?: boolean | JSX.Element
-    /**
-     * Pattern to format the input content.
-     * Ex.:
-     *     pattern: (##) #####-####
-     *     value: 12345678912
-     *     result: (12) 34567-8912
-     * @default undefined
-     */
-    format: string
-    /**
-     * Character to fill the format pattern.
-     * @default undefined
-     */
-    mask?: string
-    /**
-     * Character to be replace by value
-     * @default #
-     */
-    patternChar?: string
-    /**
-     * If true, format will be applied on empty values
-     * @default false
-     */
-    allowEmptyFormatting?: boolean,
-    /**
-     * A checker function to validate the input value.
-     * @default undefined
-     */
-    isAllowed?: (values: NumberFormatValues) => boolean
-    /**
-     * This handler provides access to any values changes in the input field and is triggered only when a prop changes or the user input changes.
-     * @default undefined
-     */
-    onValueChange?: OnValueChange
-    /**
-     * All properties of the react-number-format tag must be passed inside this property
-     * @default undefined
-     */
-    inputProps?: InputElementProps
-  }
+  /**
+   * Input label with id and htmlFor
+   */
+  label?: string
+  /**
+   * Render a badge with text 'Opcional'
+   * @default false
+   */
+  optional?: boolean
+  /**
+   * Render a message text below input
+   * @default undefined
+   */
+  error?: string
+  /**
+   * Disable all component
+   * @default false
+   */
+  disabled?: boolean
+  /**
+   * Input placeholder
+   * @default undefined
+   */
+  placeholder?: string
+  /**
+   * Renders an icon to the left of the input. When an icon is not passed as a JSX element The magnifying glass icon is the default
+   * @default undefined
+   */
+  startIcon?: boolean | JSX.Element
+  /**
+   * Renders an icon to the right of the input. When an icon is not passed as a JSX element The magnifying glass icon is the default
+   * @default undefined
+   */
+  endIcon?: boolean | JSX.Element
+  /**
+   * Listen the click event in the endIcon props.
+   */
+  onClickEndIcon?: () => void
+  /**
+   * To work, value needs a number type
+   */
+  value?: number
+  /**
+   * Pattern to format the input content.
+   * Ex.:
+   *     pattern: (##) #####-####
+   *     value: 12345678912
+   *     result: (12) 34567-8912
+   * @default undefined
+   */
+  format: string
+  /**
+   * Character to fill the format pattern.
+   * @default undefined
+   */
+  mask?: string
+  /**
+   * Character to be replace by value
+   * @default #
+   */
+  patternChar?: string
+  /**
+   * If true, format will be applied on empty values
+   * @default false
+   */
+  allowEmptyFormatting?: boolean
+  /**
+   * A checker function to validate the input value.
+   * @default undefined
+   */
+  isAllowed?: (values: NumberFormatValues) => boolean
+  /**
+   * This handler provides access to any values changes in the input field and is triggered only when a prop changes or the user input changes.
+   * @default undefined
+   */
+  onValueChange?: OnValueChange
+  /**
+   * All properties of the react-number-format tag must be passed inside this property
+   * @default undefined
+   */
+  inputProps?: Partial<InputElementProps>
+}
 
-  export function MaskInput({
-    label,
-    optional,
-    error,
-    disabled,
-    placeholder,
-    inputProps,
-    startIcon,
-    endIcon,
-    format,
-    mask,
-    isAllowed,
-    onValueChange,
-    allowEmptyFormatting,
-    patternChar
-  }: MaskInputProps) {
-    const DefaultStartIcon =
-      typeof startIcon === 'boolean' && startIcon ? <SearchNormal1 /> : startIcon
-  
-    const DefaultEndIcon =
-      typeof endIcon === 'boolean' && endIcon ? <SearchNormal1 /> : endIcon
-  
-    return (
-      <InputContainer>
+export function MaskInput({
+  label,
+  optional,
+  error,
+  disabled,
+  placeholder,
+  inputProps,
+  startIcon,
+  endIcon,
+  onClickEndIcon,
+  value,
+  format,
+  mask,
+  isAllowed,
+  onValueChange,
+  allowEmptyFormatting,
+  patternChar
+}: MaskInputProps) {
+  const StartIcon =
+    typeof startIcon === 'boolean' && startIcon ? <SearchNormal1 /> : startIcon
+
+  const EndIcon =
+    typeof endIcon === 'boolean' && endIcon ? <SearchNormal1 /> : endIcon
+
+  return (
+    <InputContainer>
+      {(label || optional) && (
         <div className="flex items-center mb-1 gap-2 min-h-[1.375rem]">
-          <InputLabel htmlFor={label} title={label}>
-            {label}
-          </InputLabel>
+          {label && (
+            <InputLabel htmlFor={label} title={label}>
+              {label}
+            </InputLabel>
+          )}
           {optional && <OptionalBadge>Opcional</OptionalBadge>}
         </div>
-  
-        <InputElementContainer error={!!error} disabled={disabled}>
-          {DefaultStartIcon && DefaultStartIcon}
-  
-          <InputElement
-            {...inputProps}
-            format={format}
-            mask={mask}
-            patternChar={patternChar}
-            isAllowed={isAllowed}
-            onValueChange={onValueChange}
-            allowEmptyFormatting={allowEmptyFormatting}
-            id={label}
-            placeholder={placeholder}
-            disabled={disabled}
-          />
-  
-          {DefaultEndIcon && DefaultEndIcon}
-        </InputElementContainer>
-  
-        {error && (
-          <ErrorTextContainer>
-            <Danger size={16} />
-            <span>{error}</span>
-          </ErrorTextContainer>
+      )}
+
+      <InputElementContainer error={!!error} disabled={disabled}>
+        {StartIcon && <IconContainer>{StartIcon}</IconContainer>}
+
+        <InputElement
+          format={format}
+          mask={mask}
+          patternChar={patternChar}
+          isAllowed={isAllowed}
+          onValueChange={onValueChange}
+          allowEmptyFormatting={allowEmptyFormatting}
+          id={label}
+          placeholder={placeholder}
+          disabled={disabled}
+          value={value}
+          {...inputProps}
+        />
+
+        {EndIcon && !onClickEndIcon && <IconContainer>{EndIcon}</IconContainer>}
+        {EndIcon && onClickEndIcon && (
+          <IconContainer onClick={onClickEndIcon} className="cursor-pointer">
+            {EndIcon}
+          </IconContainer>
         )}
-      </InputContainer>
-    )
-  }
+      </InputElementContainer>
+
+      {error && (
+        <ErrorTextContainer>
+          <Danger size={16} />
+          <span>{error}</span>
+        </ErrorTextContainer>
+      )}
+    </InputContainer>
+  )
+}
--- packages/react/src/core/MaskInput/styles.ts
+++ packages/react/src/core/MaskInput/styles.ts
@@ -2,78 +2,87 @@ import { PatternFormat } from 'react-number-format'
 import { w, W } from 'windstitch'
 
 export const InputContainer = w.fieldset(
-    'border-none p-0 m-0 w-full min-w-0',
-    {}
+  'border-none p-0 m-0 w-full min-w-0',
+  {}
 )
 
 export const InputLabel = w.label(
-    `
+  `
     font-bold text-grey-800 text-xs text-ellipsis whitespace-nowrap overflow-hidden
     `,
-    {}
+  {}
 )
 
-    export const OptionalBadge = w.span(
-    `
+export const OptionalBadge = w.span(
+  `
     font-regular text-grey-400 text-xs
     py-[.125rem] px-1
     bg-grey-100 rounded
     `,
-    {}
+  {}
 )
 
-    export const InputElementContainer = w.div(
-    `
+export const InputElementContainer = w.div(
+  `
     w-full min-w-0
     flex items-center gap-4
     py-3 px-4 rounded-lg
-    border border-grey-400
-
-    text-grey-800
-
-    focus-within:border-black
-
-    [&>svg]:w-4 [&>svg]:h-4 [&>svg]:shrink-0
+    border
     `,
-    {
-        variants: {
-        error: (error: boolean) =>
-            error
-            ? 'border-assistant-red-main focus-within:border-assistant-red-main'
-            : '',
-        disabled: (disabled: boolean) =>
-            disabled
-            ? 'bg-grey-100 border-grey-600 text-grey-600 placeholder:text-grey-600'
-            : ''
-        },
-        defaultVariants: {
+  {
+    variants: {
+      error: (error: boolean) =>
+        error
+          ? 'border-assistant-red-main focus-within:border-assistant-red-main text-grey-800'
+          : '',
+      disabled: (disabled: boolean) =>
+        disabled
+          ? 'bg-grey-100 border-grey-600 text-grey-600 placeholder:text-grey-600 cursor-not-allowed'
+          : ''
+    },
+    compoundVariants: [
+      {
+        // @ts-ignore
         error: false,
-        disabled: false
-        }
+        // @ts-ignore
+        disabled: false,
+        class: 'border-grey-400 focus-within:border-black text-grey-800'
+      }
+    ],
+    defaultVariants: {
+      error: false,
+      disabled: false
     }
+  }
 )
 
-    export const InputElement = w( PatternFormat,
-    {
-        className:
-        `
+export const InputElement = w(PatternFormat, {
+  className: `
             w-full min-w-0
             bg-transparent
 
             text-sm text-inherit font-regular
-            placeholder:text-grey-600 placeholder:font-light placeholder-shown:
-        `
-    }
-)
+            placeholder:text-grey-600 placeholder:font-light
 
-    export const ErrorTextContainer = w.div(
-    `
+            disabled:cursor-not-allowed
+        `
+})
+
+export const ErrorTextContainer = w.div(
+  `
     text-xs text-assistant-red-main
     flex items-center gap-2
 
     mt-1 
     `,
-    {}
+  {}
+)
+
+export const IconContainer = w.span(
+  `
+    w-4 h-4 flex justify-center items-center shrink-0 select-none
+  `,
+  {}
 )
 
 export type InputElementProps = W.Infer<typeof InputElement>
